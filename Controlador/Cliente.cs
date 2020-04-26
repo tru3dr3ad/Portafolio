b5020159c9d478706c26c26d901adc629fd4a559
kristal <kristal_rojas10331@hotmail.com>
2020-04-26 01:29:31 -0400

    Se Agrega Clase Cliente

---
+++ Controlador/Cliente.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controlador
+{
+    public class Cliente
+    {
+        public int Rut { get; set; }
+        public char Dv { get; set; }
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public int Telefono { get; set; }
+        public int Estado { get; set; }
+        public int Tipo { get; set; }
+        public Cliente()
+        {
+
+        }
+        public Cliente(int rut, char dv, string nombre, string direccion, int telefono, int estado, int tipo)
+        {
+            Rut = rut;
+            Dv = dv;
+            Nombre = nombre;
+            Direccion = direccion;
+            Telefono = telefono;
+            Estado = estado;
+            Tipo = tipo;
+        }
+    }
+}
