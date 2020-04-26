72c4ec13e13a3088878c0485d3cd291549d1840c
kristal <kristal_rojas10331@hotmail.com>
2020-04-26 01:53:20 -0400

    Se agrega clase ConectorDALC

---
+++ Controlador/ConectorDALC.cs
@@ -0,0 +1,29 @@
+﻿using Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controlador
+{
+    public class ConectorDALC
+    {
+        private static AlmacenEntities _modeloAlmacen;
+        public static AlmacenEntities ModeloAlmacen
+        {
+            get
+            {
+                if (_modeloAlmacen == null)
+                {
+                    _modeloAlmacen = new AlmacenEntities();
+                }
+                return _modeloAlmacen;
+            }
+        }
+        public ConectorDALC()
+        {
+
+        }
+    }
+}
